16c2b27392981f41adf648d6c87e133fd5b60d27
Mikko Nylén <mikko.nylen@gmail.com>
2014-03-30 15:51:10 +0300

    Add leiningen project.clj

---
+++ project.clj
@@ -0,0 +1,5 @@
+(defproject openpassword "0.1.0"
+  :description "Open reader for 1Password vaults"
+  :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/data.codec "0.1.0"]
+                 [cheshire "5.3.1"]])
