971b67b65c43d62aefbe15dd4c9216decb7cc98b
Mikko Nylén <mikko.nylen@gmail.com>
2014-04-03 17:23:36 +0300

    Add dependency to clojurescript and lein-cljsbuild plugin

--- project.clj
+++ project.clj
@@ -1,5 +1,8 @@
 (defproject openpassword "0.1.0"
   :description "Open reader for 1Password vaults"
   :dependencies [[org.clojure/clojure "1.6.0"]
+                 [org.clojure/clojurescript "0.0-2202"]
                  [org.clojure/data.codec "0.1.0"]
-                 [cheshire "5.3.1"]])
+                 [cheshire "5.3.1"]]
+  :plugins [[lein-cljsbuild "1.0.3"]]
+  :cljsbuild {:build []})
